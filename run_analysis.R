8fd75b92821fe23b899b86bc12142b9d817a0299
irshad420 <doctoraries87@gmail.com>
2016-08-28 01:47:37 +0500

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,56 @@
+library(reshape2)
+
+filename <- "getdata_dataset.zip"
+
+## Download the file and put the file in the data folder
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
+
+## Unzip the file
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+## unzipped files are in the folderUCI HAR Dataset. Get the list of the files
+path_rf <- file.path("./data" , "UCI HAR Dataset")
+files<-list.files(path_rf, recursive=TRUE)
+files
+
+## Concatenate the data tables by rows
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+
+##set names to variables
+
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+
+## Merge columns to get the data frame Data for all data
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+
+## Subset Name of Features by measurements on the mean and standard deviation
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+
+## Subset the data frame Data by seleted names of Features
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+
+##Read descriptive activity names from "activity_labels.txt"
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+
+## Appropriately labels the data set with descriptive variable names
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+
+## Creates a second,independent tidy data set and ouput it
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
